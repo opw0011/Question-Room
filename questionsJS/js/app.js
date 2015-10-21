44d42cf5b7a764a46848834af9654589acbf264a
Alexander Wang <wngfra@gmail.com>
2015-10-21 22:20:30 +0800

    jQuery moved to app.js

--- questionsJS/js/app.js
+++ questionsJS/js/app.js
@@ -4,8 +4,28 @@
 
 /**
 * The main TodoMVC app module
+* ngSanitize added
 *
 * @type {angular.Module}
 */
 var todomvc = angular.module('todomvc', ['firebase', 'ngStorage', 'ngSanitize']);
-// added ngSanitize
+
+//jQuery
+var main = function() {
+  // activate the bootstrap tab
+  $('#tabs a').click(function (e) {
+    e.preventDefault()
+    $(this).tab('show')
+  });
+  $('#toggle_advanced').click(function (e) {
+    e.preventDefault()
+  });
+  $('#tab_post').on("hide.bs.collapse", function(){
+    $("#toggle_advanced").html('<span class="glyphicon glyphicon-collapse-down"></span> Show advanced options');
+  });
+  $('#tab_post').on("show.bs.collapse", function(){
+    $("#toggle_advanced").html('<span class="glyphicon glyphicon-collapse-up"></span> Hide advanced options');
+  });
+}
+
+$(document).ready(main);
