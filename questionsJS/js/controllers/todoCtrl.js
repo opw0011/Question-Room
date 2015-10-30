a3ee771ce6e7b5371cc386c8cd795e25e249e74e
jlpw1995 <jlpw1995@gmail.com>
2015-10-31 00:50:05 +0800

    image upload(not complete)

--- questionsJS/js/controllers/todoCtrl.js
+++ questionsJS/js/controllers/todoCtrl.js
@@ -107,50 +107,6 @@ function ($scope, $location, $firebaseArray, $sce, $localStorage, $window, $time
 		$scope.input = {wholeMsg: ""};
 	};
 
-	$scope.encodeImageFileAsURL = function(cb){
-		return function(){
-        var file = this.files[0];
-        var reader  = new FileReader();
-        reader.onloadend = function () {
-            cb(reader.result);
-        }
-        reader.readAsDataURL(file);
-    	}
-	}
-
-	$scope.dencodeImg = function(s){
-    var e = {}, i, k, v = [], r = '', w = String.fromCharCode;
-    var n = [[65, 91], [97, 123], [48, 58], [43, 44], [47, 48]];
-
-    for (z in n)
-    {
-        for (i = n[z][0]; i < n[z][1]; i++)
-        {
-            v.push(w(i));
-        }
-    }
-    for (i = 0; i < 64; i++)
-    {
-        e[v[i]] = i;
-    }
-
-    for (i = 0; i < s.length; i+=72)
-    {
-        var b = 0, c, x, l = 0, o = s.substring(i, i+72);
-        for (x = 0; x < o.length; x++)
-        {
-            c = e[o.charAt(x)];
-            b = (b << 6) + c;
-            l += 6;
-            while (l >= 8)
-            {
-                r += w((b >>> (l -= 8)) % 256);
-            }
-         }
-    }
-    return r;
-}
-
 	//filter words detector, return true is detected
 	$scope.filterWord = function($string) {
 		var str = $string;
