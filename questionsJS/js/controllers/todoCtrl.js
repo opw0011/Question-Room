b5301900df25cb5e925eb15883b10663733d8bb6
Pui Wai O <i.mpeople@gmail.com>
2015-10-31 16:22:11 +0800

    [WEB] move function from question.html to todoCtrl.js

--- questionsJS/js/controllers/todoCtrl.js
+++ questionsJS/js/controllers/todoCtrl.js
@@ -317,4 +317,28 @@ function ($scope, $location, $firebaseArray, $sce, $localStorage, $window, $time
 			$scope.postTimeCounter = 0;
 		}, 5000);
 	}
+
+	$scope.encodeImageFileAsURL = function(){
+
+    var filesSelected = document.getElementById("inputFileToLoad").files;
+    if (filesSelected.length > 0)
+    {
+      var fileToLoad = filesSelected[0];
+
+      var fileReader = new FileReader();
+
+      fileReader.onload = function(fileLoadedEvent) {
+        var srcData = fileLoadedEvent.target.result; // <--- data: base64
+
+        var newImage = document.createElement('img');
+        newImage.src = srcData;
+
+        document.getElementById("imgTest").innerHTML = newImage.outerHTML;
+        console.log(srcData);
+        //alert(document.getElementById("imgTest").innerHTML);
+        // console.log("Converted Base64 version is "+document.getElementById("imgTest").innerHTML);
+      }
+      fileReader.readAsDataURL(fileToLoad);
+    }
+}
 }]);
