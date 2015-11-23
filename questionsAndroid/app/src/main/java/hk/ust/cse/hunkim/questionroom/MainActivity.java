3dd57b94448c1918f3264b218e8e890035cfdfed
Liu Man wai <vivianliu1009@gmail.com>
2015-11-23 17:39:45 +0800

    give a header title for each room

--- questionsAndroid/app/src/main/java/hk/ust/cse/hunkim/questionroom/MainActivity.java
+++ questionsAndroid/app/src/main/java/hk/ust/cse/hunkim/questionroom/MainActivity.java
@@ -110,15 +110,26 @@ public class MainActivity extends ListActivity {
         // Setup our Firebase mFirebaseRef
         mFirebaseRef = new Firebase(FIREBASE_URL).child(roomName).child("questions");
 
-        // Edit by Erez, give a header title for each room
-
+        // give a header title for each room
+        
         TextView p = (TextView) findViewById(R.id.roomname_View);
-
+        
         if(roomName.length() > 10){
             String q = roomName.substring(0,10) + "...";
             p.setText(q);
         } else {
             p.setText(roomName);
+        }
+        
+        // Edit by Erez, give a header title for each room
+
+        TextView head = (TextView) findViewById(R.id.roomname_View);
+
+        if(roomName.length() > 10){
+            String q = roomName.substring(0,10) + "...";
+            head.setText(q);
+        } else {
+            head.setText(roomName);
         }
 
         // Setup our input methods. Enter key on the keyboard or pushing the send button
